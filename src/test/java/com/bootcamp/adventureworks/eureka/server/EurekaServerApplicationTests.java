9b99e76d7c6f063b8cbf15e58fd00647f065e276
rhurtadozegarra <rhutadozegarra@gmail.com>
2020-12-10 14:18:01 -0500

    Carga inicil eurekaserver

---
+++ src/test/java/com/bootcamp/adventureworks/eureka/server/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.adventureworks.eureka.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
